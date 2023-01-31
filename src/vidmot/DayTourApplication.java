33934375c2e8960ea55e8d48bb9ea5ee741a05c6
Einar <einarlogip@outlook.com>
2023-01-31 18:44:03 +0000

    3rd commit

--- src/vidmot/DayTourApplication.java
+++ src/vidmot/DayTourApplication.java
@@ -5,13 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class DayTripApplication extends Application {
+public class DayTourApplication extends Application {
 
     private static Scene scene;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(DayTripApplication.class.getResource("../resources/dayTrip.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(DayTourApplication.class.getResource("../resources/dayTourSearch.fxml"));
         primaryStage.setTitle("Day Trip");
         primaryStage.setScene(new Scene(fxmlLoader.load(), 1200, 900));
         primaryStage.show();
