c298ebfa2b961e83f0d5d180590550a61fa9a6f0
Einar <einarlogip@outlook.com>
2023-02-08 20:26:37 +0000

    betri dropdown

--- src/vidmot/DayTourController.java
+++ src/vidmot/DayTourController.java
@@ -1,4 +1,24 @@
 package vidmot;
 
-public class DayTourController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DayTourController implements Initializable {
+
+    @FXML private ComboBox<String> areaDropdown;
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String[] areas = {"Vesturland","Norðurland","Suðurland","Austurland"};
+
+        for (String area : areas) {
+            areaDropdown.getItems().add(area);
+        }
+        areaDropdown.setValue(areas[0]);
+    }
 }
